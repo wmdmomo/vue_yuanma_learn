153eac4d95db68445e847ef42e1475fc169d4793
minieggwang <minieggwang@tencent.com>
2021-08-05 20:17:35 +0800

    mfirst

---
+++ test.js
@@ -0,0 +1,8 @@
+// var b = 'foo'
+// console.log(typeof b)
+// d = Array.prototype.map
+//     .call(b, (item) => item.toUpperCase()).join('')
+// console.log(d)
+var str = "wgdueg bduehgud ndie"
+var res = str.split('').reverse().join('')
+console.log(res)
